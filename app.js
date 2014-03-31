232537fd6789ad52a754b6915cd16263aa068f11
Kay-Zee <kan.k.zhang@gmail.com>
2014-03-30 21:11:34 -0700

    Update routes
    /recommendations now accepts get requests
    
    /listen and /follow now accepts post request,
    
    Functionality not yet implemented

--- app.js
+++ app.js
@@ -59,6 +59,11 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+// Added functionality for client
+app.get('/recommendations', routes.recommend);
+app.post('/follow', routes.follow);
+app.post('/listen', routes.listen);
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,15 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.recommend = function(req, res){
+  res.render('index', { title: 'Recommendations' });
+};
+	
+exports.follow = function(req, res){
+  res.render('index', { title: 'Follow' });
+};
+		
+exports.listen = function(req, res){
+  res.render('index', { title: 'Listen' });
+};
